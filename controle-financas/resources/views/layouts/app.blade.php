6781334a5e89c8fc84a407e7759292ecbf86fcba
Guilherme <guilhermesouzabenso@gmail.com>
2022-11-03 19:11:55 -0300

    New: novas linhas adicionadas

--- controle-financas/resources/views/layouts/app.blade.php
+++ controle-financas/resources/views/layouts/app.blade.php
@@ -28,7 +28,6 @@
     <div class="toast-container position-fixed bottom-0 end-0 p-3">
   <div id="liveToast" class="toast" role="alert" aria-live="assertive" aria-atomic="true">
     <div class="toast-header">
-      <img src="..." class="rounded me-2" alt="...">
       <strong class="me-auto">Tudo pronto!!</strong>
       <small>Agora mesmo.</small>
       <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
